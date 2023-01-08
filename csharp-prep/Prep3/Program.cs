a855068ccf7ecee1dfc5bac42e661d85f64d3bf8
JordanPostak <jordan_postak@hotmail.com>
2023-01-08 16:49:23 -0700

    Completing C# Prep 3-Loops

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -3,7 +3,42 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep3 World!");
+   {
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        int guess = -1;
+        int number = 0;
+        string restart = "y";
+
+
+        while (restart == "y")
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (magicNumber > guess)
+            {
+                Console.WriteLine("Higher");
+                number++;
+                Console.WriteLine($"You have guessed {number} times.");
+
+            }
+            else if (magicNumber < guess)
+            {
+                Console.WriteLine("Lower");
+                number++;
+                Console.WriteLine($"You have guessed {number} times.");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+                number++;
+                Console.WriteLine($"You have guessed {number} times.");
+                Console.Write("Do you want to continue? ");
+                restart = Console.ReadLine();
+            }
+
+        }                    
     }
 }
