ec7d6f5f534c3ea31d05991ef10199f7bfbde011
JordanPostak <jordan_postak@hotmail.com>
2023-02-05 12:50:29 -0700

    fixing comments

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -3,7 +3,7 @@ using System;
 
 // The program exceeds the core requirements in two ways
 //   1. A challenge people might have is memorizing the right scripture. 
-//      I have provided a menu so that the user can select the right scripture.
+//      I have provided a menu so that the user can select the right scripture Or get a random scripture.
 //   2. This program works with a library of scriptures rather than a single one. 
 //      Rather than choose scriptures at random the user can select the right one.
 
@@ -22,12 +22,14 @@ class Program
             new Scripture(new Reference("Moroni 7:45"), new Text("And charity suffereth long, and is kind, and envieth not, and is not puffed up, seeketh not her own, is not easily provoked, thinketh no evil, and rejoiceth not in iniquity but rejoiceth in the truth, beareth all things, believeth all things, hopeth all things, endureth all things.")),
             new Scripture(new Reference("Abraham 3:22-23"), new Text("Now the Lord had shown unto me, Abraham, the intelligences that were organized before the world was; and among all these there were many of the noble and great ones;\nAnd God saw these souls that they were good, and he stood in the midst of them, and he said: These I will make my rulers; for he stood among those that were spirits, and he saw that they were good; and he said unto me: Abraham, thou art one of them; thou wast chosen before thou wast born."))
         };
-         Console.Clear();
+        Console.Clear();
         Console.WriteLine();
         Console.WriteLine("Welcome to Scripture Memorizer!");
         Console.WriteLine();
         
         // Display initial menu
+        // A challenge people might have is memorizing the right scripture. 
+        // I have provided a menu so that the user can select the right scripture Or get a random scripture.
         Console.WriteLine("Would you like to:");
         Console.WriteLine();
         Console.WriteLine("1. Select a scripture");
@@ -43,6 +45,9 @@ class Program
         case 1:
         Console.Clear();
         // Code for selecting a scripture
+        // I have provided a menu so that the user can select the right scripture.
+        // This program works with a library of scriptures rather than a single one. 
+        // Rather than choose scriptures at random the user can select the right one.
         Console.WriteLine();
         Console.WriteLine("Here is a list of scriptures:");
         Console.WriteLine();
@@ -62,6 +67,7 @@ class Program
         case 2:
         Console.Clear();
         // Code for randomly selecting a scripture
+        // I have provided a menu so that the user can get a random scripture.
         int randomIndex = rand.Next(scriptures.Length);
         Scripture randomScripture = scriptures[randomIndex];
         // Create an instance of the Memorizer class
